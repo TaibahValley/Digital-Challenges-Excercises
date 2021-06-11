36a35c34e938904479c64a8c970a6c9de09ae2ce
Raghad Abdulghani <raghad.at@gmail.com>
2021-06-11 18:08:58 +0300

    Create loops.sol

---
+++ loops.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.5.0; 
+   
+contract Types { 
+    
+    uint[] data; 
+  
+  //for loop 
+    function forLoop(
+    ) public returns(uint[] memory){
+    for(uint i=0; i<5; i++){
+        data.push(i);
+     }
+      return data;
+    }
+    
+    //while loop
+    uint j;
+    function whileLoop(
+    ) public returns(uint[] memory){
+    while(j < 5) {
+        j++;
+        data.push(j);
+     }
+      return data;
+    }
+    
+    }
